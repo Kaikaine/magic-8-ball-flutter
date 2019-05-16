c279851fa5ae481d9713def35f8170b35606c6f9
kaikaine <kairijb@protonmail.com>
2019-05-16 13:58:12 -0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            backgroundColor: Colors.blue[900],
+            title: Text(
+              'Ask Me Anything',
+            ),
+          ),
+          body: Container(),
+        ),
       ),
     );
